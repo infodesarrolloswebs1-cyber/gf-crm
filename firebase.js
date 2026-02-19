2216e97ab7646d5e8b68c72ac116b731e887a47f
infodesarrolloswebs1-cyber <info.desarrollos.webs1@gmail.com>
2026-02-18 21:33:42 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJbG9uZqqdCJai2fN3aBP_2cAcYxUlyw0",
+  authDomain: "gf-desarrollos-crm.firebaseapp.com",
+  projectId: "gf-desarrollos-crm",
+  storageBucket: "gf-desarrollos-crm.firebasestorage.app",
+  messagingSenderId: "946811439846",
+  appId: "1:946811439846:web:acf6ba394a517ae11c8927"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);

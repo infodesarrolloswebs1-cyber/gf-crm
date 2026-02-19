565c205ecb3a0f32256b46d9aaf25804f52b47f0
infodesarrolloswebs1-cyber <info.desarrollos.webs1@gmail.com>
2026-02-18 23:04:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
-import { db } from "./firebase.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { db, auth } from "./firebase.js";
 
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+// LOGIN
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("pass").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, pass);
+    alert("Login correcto");
+  } catch (e) {
+    alert("Error login");
+    console.log(e);
+  }
+};
+
+
+// CREAR CLIENTE
 window.crearCliente = async function () {
   try {
+
     await addDoc(collection(db, "clientes"), {
       nombre: "Cliente prueba",
-      email: "test@mail.com",
       fecha: new Date()
     });
 
-    alert("Cliente guardado en Firebase 🚀");
+    alert("Cliente guardado");
+
   } catch (e) {
-    console.error(e);
-    alert("Error");
+    console.log(e);
+    alert("Error al guardar");
   }
 };
